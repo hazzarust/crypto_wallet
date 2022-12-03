44065ccf788a554bb4b7faafedc107aa27d20e30
Harry <harry.grant125@gmail.com>
2022-12-03 15:39:29 +0000

    uncommented

--- src/main.rs
+++ src/main.rs
@@ -9,30 +9,28 @@ use dotenv; //loads enviornmental variables from .env file, mashes variables wit
             //environmental variables are essentially global variables and can be created and available for reference at a point in time. 
 
 
-//I HAVE COMMENTED OUT THE CODE BELOW AS I DO NOT WISH TO CREATE A BRAND NEW SECRET KEY EVERY TIME I TRANSACT 
-//THE BELOW COULD BE ITS OWN SMALL PROGRAM IF REQURIED, I WROTE IT IN HERE AND COMMENTED OUT FOR PRACTICE PURPOSES
-//IF YOU ARE RUNNING THIS PROGRAM FOR THE FIRST TIME, PLEASE UNCOMMENT EVERYTHING THATS NOT A LEGIT CODE IN MAIN TO GENERATE A KEYPAIR AND ADDRESS
+
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenv::dotenv().ok();
-    //let (secret_key, pub_key) = eth_wallet::generate_keypair(); //calls the generate_keypair function from eth_wallet and stores tuple 
+    let (secret_key, pub_key) = eth_wallet::generate_keypair(); //calls the generate_keypair function from eth_wallet and stores tuple 
 
-   // println!("secret key: {}", &secret_key.to_string());
-    //println!("Public key: {}", &pub_key.to_string());
+    println!("secret key: {}", &secret_key.to_string());
+    println!("Public key: {}", &pub_key.to_string());
 
-    //let pub_address = eth_wallet::public_key_address(&pub_key);
-    //println!("Public Key Address {}", pub_address);
+    let pub_address = eth_wallet::public_key_address(&pub_key);
+    println!("Public Key Address {}", pub_address);
 
-    //let crypto_wallet = eth_wallet::Wallet::new(&secret_key, &pub_key);
-    //println!("{:?}", crypto_wallet);
+    let crypto_wallet = eth_wallet::Wallet::new(&secret_key, &pub_key);
+    println!("{:?}", crypto_wallet);
 
-    //crypto_wallet.save_to_file("crypto_wallet.json")?; //makes a json file with the crypto wallet struct inside
+    crypto_wallet.save_to_file("crypto_wallet.json")?; //makes a json file with the crypto wallet struct inside
 
     let wallet_file_path = "crypto_wallet.json";
-    //println!("{:?}", wallet_file_path);
+    println!("{:?}", wallet_file_path);
 
     let loaded_wallet = eth_wallet::Wallet::from_file(&wallet_file_path)?;
-    //println!("Loaded Wallet: {:?}", loaded_wallet);
+    println!("Loaded Wallet: {:?}", loaded_wallet);
 
 
     //ALLOWS US TO CONNECT TO GOERLI TESTNET
